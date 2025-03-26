e6699752f061a364436bf07a8053bcd3dc066743
BhandariPratik <pratikbhandari00722@gmail.com>
2025-03-26 12:35:59 +0530

    made change to as it was

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -2,18 +2,15 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Routes
-// import Auth from "./components/auth.jsx";
-// import Dashboard from "./components/dashboard.jsx";
-// import Recipedetails from "./components/recipe-details.jsx";
-import Auth from "@/components/auth.jsx";
-import Dashboard from "@/components/dashboard.jsx";
-import Recipedetails from "@/components/recipe-details.jsx";
+import Auth from "./components/Auth"
+import Dashboard from "./components/Dashboard";
+import RecipeDetails from "./components/RecipeDetails";
 
 
 //middleware 
 import ProtectedRoute from "./utils/protected.routes.jsx";
 import { useAuth0 } from "@auth0/auth0-react";
-import Header from "./utils/Header.jsx";
+import Header from "./utils/Header";
 
 const App = () => {
   const { isAuthenticated, isLoading } = useAuth0();
@@ -26,7 +23,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={ <Auth /> } />
         <Route path="/dashboard" element={ <ProtectedRoute><Dashboard /></ProtectedRoute> } />
-        <Route path="/recipeDetails/" element={ <ProtectedRoute><Recipedetails /></ProtectedRoute> } />
+        <Route path="/recipeDetails/" element={ <ProtectedRoute><RecipeDetails /></ProtectedRoute> } />
       </Routes>
     </Router>
   );
